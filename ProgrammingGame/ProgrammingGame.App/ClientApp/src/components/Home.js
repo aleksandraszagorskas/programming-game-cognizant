15405ff0a181c9f7420db52f52ad71eb6c3fc114
Aleksandras Zagorskas <aleksandras.zagorskas@gmail.com>
2021-08-07 19:43:13 +0300

    ui form

--- ProgrammingGame/ProgrammingGame.App/ClientApp/src/components/Home.js
+++ ProgrammingGame/ProgrammingGame.App/ClientApp/src/components/Home.js
@@ -5,22 +5,41 @@ export class Home extends Component {
 
   render () {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
+        <div class="container">
+            <form>
+                <div class="form-group row">
+                    <label for="participant-name" class="col-sm-3 col-form-label">NAME</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="participant-name"/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="task-select" class="col-sm-3 col-form-label">SELECT TASK</label>
+                    <div class="col-sm-9">
+                        <select class="form-control" id="task-select">
+                            <option>Hello world task</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="description-textarea" class="col-sm-3 col-form-label">DESCRIPTION</label>
+                    <div class="col-sm-9">
+                        <textarea readonly class="form-control-plaintext" id="description-textarea" rows="5" value="Test task description"></textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="description-textarea" class="col-sm-3 col-form-label">SOLUTION CODE</label>
+                    <div class="col-sm-9">
+                        <textarea class="form-control-plaintext" id="description-textarea" rows="7" ></textarea>
+                    </div>
+                </div>
+                <div class="form-group row justify-content-end">
+                    <div class="col-sm-9">
+                        <button type="submit" class="btn btn-primary">SUBMIT</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     );
   }
 }
